b841243f6b8e495bf8d8fb91aa0d395677279058
zbbright <69168749+zbbright@users.noreply.github.com>
2021-03-08 15:12:40 -0500

    added Resume link to 'about'

--- src/Components/resume/Resume.js
+++ src/Components/resume/Resume.js
@@ -4,6 +4,7 @@ function Resume(props){
     return (
         <div className="container-sm mx-auto my-3 rounded bg-white">
             <h6 className="text-danger">**Content may not display correctly on small devices**</h6>
+            <p>View on Google Docs <a href="https://docs.google.com/document/d/1_dYtBHfcFPEggzEstGfqmKrBDL1cPMb1hUVs6ZLtYiA/edit?usp=sharing">here.</a></p>
             <iframe title="resume" src="https://docs.google.com/document/d/e/2PACX-1vQVfhmKGYWCDDNMxP10fCP3PgOpbjx1M4XfTs18-XL-07dNT67SXCGbiIY_gNeIqA29ZaruAiHJVej7/pub" width="100%" height="600px"/>
         </div>
     )
