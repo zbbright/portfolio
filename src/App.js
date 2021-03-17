a0c32f265cef8f6453dc3d33cfa85813e67934ea
zbbright <69168749+zbbright@users.noreply.github.com>
2021-03-16 21:01:45 -0400

    Switched to HashRouter, added 404 component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './Components/home/Home';
 import About from './Components/about/About';
 import Contact from './Components/contact/Contact';
@@ -7,18 +7,20 @@ import Projects from './Components/projects/Projects';
 import Resume from './Components/resume/Resume';
 import NavigationHead from './Components/navigation/NavigationHead.js';
 import NavigationFoot from './Components/navigation/NavigationFoot';
+import Page404 from './Components/404/Page404';
 
 function App() {
   return (
     <div className='app'>
         <NavigationHead />
-        
-        <Route exact path="/portfolio"  component={Home} />
-        <Route path="/portfolio/about"  component={About} />
-        <Route path="/portfolio/contact"  component={Contact} />
-        <Route path="/portfolio/projects"  component={Projects} />
-        <Route path="/portfolio/resume"  component={Resume} />
-
+        <Switch>
+          <Route exact path="/portfolio"  component={Home} />
+          <Route exact path="/portfolio/about"  component={About} />
+          <Route exact path="/portfolio/contact"  component={Contact} />
+          <Route exact path="/portfolio/projects"  component={Projects} />
+          <Route exact path="/portfolio/resume"  component={Resume} />
+          <Route path="/" component={Page404} />
+        </Switch>
         <NavigationFoot />
     </div>
   );
