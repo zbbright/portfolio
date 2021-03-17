5e1ab8292001743b1b294d9acb8a6abb149ac592
zbbright <69168749+zbbright@users.noreply.github.com>
2021-03-16 21:20:25 -0400

    Fixed links

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -8,7 +8,7 @@ function About(props){
         <p>After an early career pursuing firefighting and practicing high-pressure problem-solving in an fast-paced, teamwork-intensive environment, I transitioned careers.  <br/>
         Before deciding to pursue tech, a position in Amazon Fulfillment gave me the opportunity to explore tech through a special project I worked on <strong><a className='opacity-2' href="https://github.com/zbbright/amazon-project">(GitHub)</a></strong>. 
         I was in a position watching the orders coming in and ensuring the the inventory to fill them would not be affected by issues within the warehouse (inventory was moved by automated robots that would become obstructed from time to time). The process to ensure availibility was tedious, so I created an Excel workbook to automate most of the steps. The process of teaching myself to "code" an Excel program had me hooked, so when I had the opportunity to start a coding bootcamp I decided to go for it. I'm looking forward to put my new skills to work at a first tech job.</p>
-        <Link className="nav-link" to="/portfolio/resume" >View resumé</Link>
+        <Link className="nav-link" to="/resume" >View resumé</Link>
   </div>
     )}
 
--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -18,7 +18,7 @@ function Home(props){
                 <h3>I started my journey into tech in July, 2020...</h3>
                 <p>Through Kenzie Academy, I've learned Frontend Development, with a solid understanding of fundamentals for Javascript, React, HTML, and CSS.<br />I am excellent at picking up new concepts and would love to gain more exposure with your company.</p>
                 <p>I grew up in Colorado, and made a recent move to the Indianapolis area. While I love the mountains and blue skies, I'm looking to stay in the Indy Metro area at either a local office or remotely.</p>
-                <p><Link className="nav-link" to="/portfolio/about" >Learn more about me</Link></p>
+                <p><Link className="nav-link" to="/about" >Learn more about me</Link></p>
             </div>
         </div>
     </div>
@@ -28,7 +28,7 @@ function Home(props){
         <div className="card-body shadow">
             <h3>Curious about my tech skills?</h3>
             <p>Through my coding bootcamp, I've been a top-performer, graduating with grades above 100%.  I have a solid grasp on the languages I've been exposed to, and have pushed myself to learn the concepts that lie just beyond the material taught, such as deploying usable websites like the one you're accessing or teaching myself to use the Bootstrap 4 style library.<br/><br/>My tech skills include single-page React Apps, Javascript programming, Bootstrap 4, HTML, and CSS.  I have experience with remote work through six-month, fully remote Frontend Web Developer certification bootcamp, utilizing standard tools like Slack, Zoom, and Google Docs.</p>
-            <p><Link className="nav-link" to="/portfolio/projects">See some of my recent projects</Link>or <Link className="nav-link" to="/portfolio/resume">View my resume</Link></p>
+            <p><Link className="nav-link" to="/projects">See some of my recent projects</Link>or <Link className="nav-link" to="/resume">View my resume</Link></p>
         </div>
       </div>
   </div>
@@ -37,7 +37,7 @@ function Home(props){
         <div className="card-body shadow">
             <h3>Reach out!</h3>
             <p>Interested in me for <em>your</em> team? Let's see if we're a potential fit!</p>
-            <p><Link className="nav-link" to="/portfolio/contact">Contact me Here</Link></p>
+            <p><Link className="nav-link" to="/contact">Contact me Here</Link></p>
         </div>
       </div>
   </div>
